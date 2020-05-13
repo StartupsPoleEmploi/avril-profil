a25a63de7f678fd092d3a98830e64020ab85c90a
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-05-13 17:56:17 +0200

    refacto aside nav

--- components/Navigation-Dashboard.vue
+++ components/Navigation-Dashboard.vue
@@ -5,7 +5,7 @@
       <AvrilLogo/>
     </header>
 
-    <aside class="menu">
+    <div class="menu">
       <ul class="menu-list">
         <li>
           <nuxt-link to="/mon-compte">
@@ -23,7 +23,7 @@
           </nuxt-link>
         </li>
       </ul>
-    </aside>
+    </div>
 
   </aside>
 </template>
@@ -39,19 +39,38 @@ export default {
 
 <style scoped lang="scss">
 @import '~avril/scss/variables';
-.menu {
-  margin-top: 3rem;
-  &-list {
-    padding-left: 1rem;
-    line-height: 2.25;
-    a.is-active, a.is-active-parent {
-      font-weight: 600;
-      background: palette(default, background);
-      color: palette(default, text);
+
+aside.dashboard-navigation {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: $dashboard-navigation-width;
+  background-color: $white;
+  z-index: 100;
+  @include touch {
+    display: none;
+  }
+
+  .dashboard-navigation-header {
+    text-align: center;
+  }
+
+  .menu {
+    margin-top: 3rem;
+    .menu-list {
+      line-height: 2.25;
+      li a {
+        padding-left: 1rem;
+        &.is-active, &.is-active-parent {
+          font-weight: 600;
+          background: palette(default, background);
+          color: palette(default, text);
+        }
+      }
     }
   }
 }
-.dashboard-navigation-header {
-  text-align: center;
-}
+
+
 </style>
