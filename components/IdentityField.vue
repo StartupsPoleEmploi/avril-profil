e37bd34daaee169a0227d912833bf629465d2876
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-05-18 22:21:51 +0200

    remove logs

--- components/IdentityField.vue
+++ components/IdentityField.vue
@@ -80,7 +80,6 @@
       }
     },
     data: function() {
-      console.log('yo', process.env.algoliaCredentials)
       return {
         credentials: process.env.algoliaCredentials,
         types: INPUT_TYPES,
