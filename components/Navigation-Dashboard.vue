7ebd84ebe1e3758549c1b76d4366c51ae5a5401e
Augustin Riedinger <augustin.riedinger@gmail.com>
2020-06-11 18:22:24 +0200

    better navigation dashboard

--- components/Navigation-Dashboard.vue
+++ components/Navigation-Dashboard.vue
@@ -1,12 +1,23 @@
 <template>
   <aside class="dashboard-navigation">
     <div class="menu">
+      <p class="menu-label is-username">
+        {{username}}
+      </p>
+      <p class="menu-label">
+        Mon profil
+      </p>
       <ul class="menu-list">
         <li>
           <nuxt-link to="/mon-compte">
-            Mon compte
+            Mon identité
           </nuxt-link>
         </li>
+      </ul>
+      <p class="menu-label">
+        Mes VAE
+      </p>
+      <ul class="menu-list">
         <li>
           <nuxt-link to="/mes-candidatures">
             Mes candidatures
@@ -23,6 +34,17 @@
   </aside>
 </template>
 
+<script type="text/javascript">
+
+  export default {
+    computed: {
+      username() {
+        return this.$store.getters['identity/username'];
+      },
+    }
+  }
+</script>
+
 <style scoped lang="scss">
 @import '~avril/scss/variables';
 
@@ -38,10 +60,21 @@ aside.dashboard-navigation {
 
   .menu {
     margin-top: 7.25rem;
+
+    .menu-label {
+      padding-left: 1rem;
+      &.is-username {
+        font-weight: bold;
+        margin-bottom: 3rem;
+        font-size: 1rem;
+        padding-right: 1rem;
+      }
+    }
+
     .menu-list {
       line-height: 2.25;
       li a {
-        padding-left: 1rem;
+        padding-left: 2rem;
         &.is-active, &.is-active-parent {
           font-weight: 600;
           background: palette(default, background);
