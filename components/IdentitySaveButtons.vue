8bc90d45781c255b2588472548ee0622798681f5
Augustin Riedinger <augustin.riedinger@gmail.com>
2022-08-24 13:57:55 +0200

    warning if identity unsaved

--- components/IdentitySaveButtons.vue
+++ components/IdentitySaveButtons.vue
@@ -3,15 +3,17 @@
     <div class="level">
       <div class="level-left">
         <ApiButton
+          ref="saveButton"
           type="submit"
           :query="{store: 'identity', name: 'updateIdentity', type: 'identity', params: {input: savableState}}"
           disable-with="Enregistrement ..."
           class="button is-primary is-rounded is-medium"
           :to="to"
-        >Enregistrer</ApiButton>
+        >{{canUndo ? 'Enregistrer' : 'Continuer'}}</ApiButton>
       </div>
       <div class="level-right">
         <ApiButton
+          ref="resetButton"
           v-if="canUndo"
           type="reset"
           query="identity"
@@ -32,6 +34,15 @@
       },
       savableState: function() {
         return this.$store.getters['identity/savableState'];
+      }
+    },
+    beforeDestroy() {
+      if (this.canUndo) {
+        const shouldSave = window.confirm(`
+Souhaitez-vous enregistrer ces modifications ?
+Pour poursuivre n'oubliez pas d'enregistrer
+        `);
+        this.$refs[shouldSave ? 'saveButton' : 'resetButton'].$el.click();
       }
     },
     components: {
