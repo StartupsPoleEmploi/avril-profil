3701b50bb40967872e1b4ecde2e951adbcb05a6b
Augustin Riedinger <augustin.riedinger@gmail.com>
2022-12-07 18:29:11 +0100

    increase delay

--- middleware/remove-feedback.js
+++ middleware/remove-feedback.js
@@ -2,7 +2,7 @@ import get from 'lodash.get';
 
 export default async function ({ store, route }) {
   const timestamp = get(store.state, 'serverFeedback.timestamp');
-  if (timestamp && (Date.now() - timestamp > 1000)) {
+  if (timestamp && (Date.now() - timestamp > 2000)) {
     store.commit('removeFeedback');
   }
 }
